1addbaefa7ca0719bc398f1ec97e2f9b829fbe35
Valeria Kozlova <kozlova.valeriia@gmail.com>
2018-11-27 14:36:17 +0000

    Created a basic skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React, {Component} from 'react';
+import MessageList from './MessageList.jsx';
+import ChatBar from './ChatBar.jsx';
+
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <MessageList />
+        <ChatBar />
+      </div>
     );
   }
 }
